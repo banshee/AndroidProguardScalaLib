335f55c9c7f59f280fa239a050faaa6d1b056d5f
James Moore <james@restphone.com>
2012-02-23 18:22:18 -0800

    Improved error messages for nulls when figuring out location of class files

--- src/main/scala/com/restphone/androidproguardscala/ProguardCacheBuilder.scala
+++ src/main/scala/com/restphone/androidproguardscala/ProguardCacheBuilder.scala
@@ -1,19 +1,24 @@
 package com.restphone.androidproguardscala
 
-import com.restphone.androidproguardscala.jruby.JrubyEnvironmentSetup._
-import com.restphone.androidproguardscala.jruby.ProguardCache
+import org.jruby.Ruby
 import org.objectweb.asm.Type
+
+import com.restphone.androidproguardscala.jruby.JrubyEnvironmentSetup.addJarToLoadPathAndRequire
+import com.restphone.androidproguardscala.jruby.JrubyEnvironmentSetup.addJrubyJarfile
+import com.restphone.androidproguardscala.jruby.JrubyEnvironmentSetup.addToLoadPath
 import com.restphone.androidproguardscala.jruby.ProguardCacheJavaInterop
+
+import proguard.Initializer
 
 object ProguardCacheBuilder {
   /**
-   * @param directoryContainingJrubyLibraries Full path to the directory contain the jruby libraries
+   * @param directoryContainingJrubyLibraries Full path to the directory containing proguard_lib.rb
    * @return The cache controller
    */
   def buildCacheController(directoryContainingJrubyLibraries: String) = {
     addJrubyJarfile(pathForJarFileContainingClass(classOf[org.jruby.Ruby]))
 
-    List(
+    Iterable(
       classOf[org.objectweb.asm.Type],
       classOf[proguard.Initializer],
       classOf[List[String]]) foreach { loadClassIntoJRuby(_) }
@@ -27,12 +32,18 @@ object ProguardCacheBuilder {
   }
 
   private def pathForJarFileContainingClass[T](c: Class[T]) = {
-    c.getProtectionDomain.getCodeSource.getLocation.getPath
+    val calculation = for {
+      protectionDomain <- NotNull(c.getProtectionDomain, "failed on getProtectionDomain")
+      codeSource <- NotNull(protectionDomain.getCodeSource, "failed on getCodeSource, protection domain is " + protectionDomain)
+      location <- NotNull(codeSource.getLocation, "failed on getLocation " + codeSource)
+      path <- NotNull(location.getPath, "failed on getPath")
+    } yield path
+    calculation.get
   }
 
   private def loadClassIntoJRuby[T](c: Class[T]) = {
-    val p = pathForJarFileContainingClass(c)
-    loadJarIntoJRuby(p)
+    ((pathForJarFileContainingClass[T] _) andThen
+      loadJarIntoJRuby)(c)
   }
 
   private def loadJarIntoJRuby(path: String) = {
