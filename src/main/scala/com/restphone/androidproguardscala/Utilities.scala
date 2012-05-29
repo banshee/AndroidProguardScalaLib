f381f5478a1e131fa900a4eabfba7ff0f5578701
James Moore <james@restphone.com>
2012-05-28 19:29:42 -0700

    Moved RichPath to Utilities

--- src/main/scala/com/restphone/androidproguardscala/Utilities.scala
+++ src/main/scala/com/restphone/androidproguardscala/Utilities.scala
@@ -2,6 +2,7 @@ package com.restphone.androidproguardscala
 
 import scala.util.control.Exception._
 import java.io.File
+import org.eclipse.core.runtime.IPath
 
 object NotNull {
   val catchNull = catching(classOf[NullPointerException])
@@ -27,5 +28,19 @@ object RichFile {
   }
   def ensureDirExists(f: File) =
     if (!f.exists) f.mkdir
+}
+
+class RichPath(p: IPath) {
+  def /(that: String) = p.append(that)
+}
+
+object RichPath {
+  implicit def toRichPath(p: IPath): RichPath = new RichPath(p)
+//  implicit def convertFileToPath(f: java.io.File): IPath = Path.fromOSString(f.toString)
+//  implicit def convertUrlToPath(u: java.net.URL) = {
+//    val x = Platform.resolve(u)
+//    new Path(x.getFile)
+//  }
+  def ensureDirExists(p: IPath) = RichFile.ensureDirExists(p.toFile)
 }
 
