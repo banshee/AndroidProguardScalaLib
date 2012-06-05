ad86d24ca8dafeac74dbab7a6425209fc255971a
James Moore <james@restphone.com>
2012-06-04 22:41:31 -0700

    Move richpath back to the eclipse project

--- src/main/scala/com/restphone/androidproguardscala/Utilities.scala
+++ src/main/scala/com/restphone/androidproguardscala/Utilities.scala
@@ -2,7 +2,6 @@ package com.restphone.androidproguardscala
 
 import scala.util.control.Exception._
 import java.io.File
-import org.eclipse.core.runtime.IPath
 
 object NotNull {
   val catchNull = catching(classOf[NullPointerException])
@@ -15,10 +14,6 @@ object NotNull {
   }
 }
 
-class RichFile(f: File) {
-  def /(that: String) = new java.io.File(f, that)
-}
-
 object RichFile {
   def slurp(f: File) = {
     val s = scala.io.Source.fromFile(f)
@@ -29,18 +24,3 @@ object RichFile {
   def ensureDirExists(f: File) =
     if (!f.exists) f.mkdir
 }
-
-class RichPath(p: IPath) {
-  def /(that: String) = p.append(that)
-}
-
-object RichPath {
-  implicit def toRichPath(p: IPath): RichPath = new RichPath(p)
-//  implicit def convertFileToPath(f: java.io.File): IPath = Path.fromOSString(f.toString)
-//  implicit def convertUrlToPath(u: java.net.URL) = {
-//    val x = Platform.resolve(u)
-//    new Path(x.getFile)
-//  }
-  def ensureDirExists(p: IPath) = RichFile.ensureDirExists(p.toFile)
-}
-
