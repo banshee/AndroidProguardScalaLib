710855d62e03609996f0f2a1ba732c64d4bda628
James Moore <james@restphone.com>
2012-11-26 23:17:02 -0800

    more cache changes

--- src/test/com/restphone/androidproguardscala/FullRunTest.scala
+++ src/test/com/restphone/androidproguardscala/FullRunTest.scala
@@ -15,7 +15,6 @@ import com.google.common.base.Charsets
 
 class FullRunTest extends FunSuite with ShouldMatchers {
   test( "should test to see if the library needs to be built" ) {
-    CacheSystem.findExistingLibraryMatching(testConfiguration)
   }
   
   test( "should build the library if necessary, keeping a cache copy and also putting it in the destination" )( pending )
